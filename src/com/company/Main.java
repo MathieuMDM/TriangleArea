4184f6b366227524cdd5ce3d523dd95e8d277481
Mathieu Pawlicha <mateusz.pawlicha.gmail.com>
2022-09-11 10:15:25 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter three points for a triangle: ");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double x3 = input.nextDouble();
+        double y3 = input.nextDouble();
+
+        double s1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+        double s2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
+        double s3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
+        double s = (s1 + s2 + s3) / 2;
+        double area = Math.pow(s * (s - s1) * (s - s2) * (s - s3), 0.5);
+
+        System.out.println("The area of the triangle is " + area);
+    }
+}
